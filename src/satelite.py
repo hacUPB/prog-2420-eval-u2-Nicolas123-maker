12ff00a3fa55e497a906144b90e95cc48fe61171
Nicolas123-Maker <nicolas.bricnom.col@upb.edu.co>
2024-09-12 06:48:20 -0500

    Agregue mas codigo al problema del satelite

--- src/satelite.py
+++ src/satelite.py
@@ -2,22 +2,31 @@
 def main():
     An = float(input("Ingrese la altitud inicial:"))
     As = float(input("ingrese la altitud de seguridad:"))
-    K = float(input("Ingrese el coeficiente de arrastre"))
+    K = float(input("Ingrese el coeficiente de arrastre:"))
     d = An
+    z = K
     orbita = 1
     while d > As:
         p = K * d
         d = d - p
-        k += 0.0001
+        z += 0.0001
         orbita += 1
-        print(f"en la orbita {orbita}, se tuvouna altitud de {d} y un coeficiente de arrastre de {k} ")
+        if d > As:
+            print(f"en la orbita {orbita}, se tuvo una altitud de {d} y un coeficiente de arrastre de {z} ")
         if p < 0.000001:
             d = 0
             print(f"El satelite se ha estabilizado despues de {orbita} orbitas")
         elif d < As:
             print(f"El satelite se ha desintegrado en la atmosfera despues de {orbita} orbitas")
-        else:
-            pass # borra esta línea cuando con inicies tu código
+            opcion = input("le gustaria conocer la altitud en la que el satelite se desintegro? :")
+            opcion.upper() = opcion
+            if opcion not in ["Si, NO"]:
+                print("opcion no valida ._.")
+            elif opcion == "Si":
+                print(f"el satelie se desintegro a una altitud de {d}")
+            else:
+            
+                pass 
 
 
 if __name__ == "__main__":
