0c645e624a7b12d76a75633fd8e4b5f07f06f213
Nicolas123-Maker <nicolas.bricnom.col@upb.edu.co>
2024-09-15 03:05:01 -0500

    Cambio 2

--- src/reservas.py
+++ src/reservas.py
@@ -56,33 +56,55 @@ def main():
     else:
       print("Excelente elección!, le recomendamos visitar el jardín botanico.")
     d = 240
-    print(f"entre las ciudades hay una distancia de {d}.")
+    print(f"entre las ciudades hay una distancia de {d}km.")
   elif origen == "medellin" and destino == "cartagena" or origen == "cartagena" and destino == "medellin":
     d = 461
     if origen == "medellin" and destino == "cartagena":
       print("Excelente elección!, recuerde llevar bloqueador pal' sol.")
     else:
       print("Excelente elección!, le recomendamos visitar el jardín botanico.")
-    print(f"entre las ciudades hay una distancia de {d}.")
+    print(f"entre las ciudades hay una distancia de {d}km.")
   elif origen == "bogota" and destino == "cartagena" or origen == "cartagena" and destino == "bogota":
     d = 657
     if origen ==  "bogota" and destino == "cartagena":
       print("Excelente elección!, recuerde llevar bloqueador pal' sol.")
     else: print("Excelente elección!, recuerde llevar abrigo pal' frio.")
-    print(f"entre las ciudades hay una distancia de {d}.")
+    print(f"entre las ciudades hay una distancia de {d}km.")
+  fecha = datetime.datetime.now()
+  fecha_control = datetime.datetime.strftime(fecha, "%d/%m/%Y")
   #Cuarto while de control
   while v4_control == "true":
-    Fecha_vuelo = str(input("Por favor ingrese la fecha en la que quiere agendar su vuelo en formato AA/MM/DÍA (Para ingresar numeros menores que 10 poga un cero antes del numero): "))
-    fecha_control = datetime.today()
-    if Fecha_vuelo < fecha_control:
+    Fecha_vuelo = input("Por favor ingrese la fecha en la que quiere agendar su vuelo en formato DD/MM/AA \n (Para ingresar numeros menores que 10 ponga un cero antes del numero): ")
+
+    if Fecha_vuelo > fecha_control:
       print("por favor ingrese una fecha valida")
     else: v4_control = "false"
-    
-          
+  Dia_de_la_semana = Fecha_vuelo.weekday()
+  lista_A = [0, 1, 2, 3]
+  lista_B = [4, 5, 6]
+  if d < 400 and Dia_de_la_semana not in lista_B :
+    precio = 79,900
+  elif d < 400 and Dia_de_la_semana not in lista_A:
+    precio = 119,900
+  elif d >= 400 and Dia_de_la_semana not in lista_B:
+    precio = 156,900
+  elif d >= 400 and Dia_de_la_semana not in lista_A:
+    precio = 213,000      
+  while True:
+    print("Seleccione C para pasillo.\nSeleccione V para ventana.\nSeleccione S si no tiene prferencia.")
+    Asiento_pref = str(input(f"{titulo}. {nombre}, por favor Escoja su asiento según su preferencia:"))
+    Asiento_pref = Asiento_pref.upper()
+    Lista_pref = ["C", "V", "S"]
+    if Asiento_pref not in Lista_pref:
+      print("Por favor seleccione correctamente su preferencia.")
+      False
+  Num_asiento = random.randint(1,30)
 
 
 
-    pass
+
+
+  pass
         
 
 if __name__ == "__main__":
