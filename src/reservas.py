485e6acc314604c117c365057eb745d4becfa652
Nicolas123-Maker <nicolas.bricnom.col@upb.edu.co>
2024-09-15 01:26:44 -0500

    corregi detalles del codigo de las reservas

--- src/reservas.py
+++ src/reservas.py
@@ -1,11 +1,14 @@
 from os import system
 from random import randint
+import datetime
+import time
 def main():
   #conjunto de definición de las variables de control
   c ="la arepa que vuela"
   v1_control = "true"
   v2_control = "true"
   v3_control = "true"
+  v4_control = "true"
   #primer while de control
   while v1_control == "true":
       titulo = str(input("Como prefiere que nos refiramos hacia usted? (sr. o sra):"))
@@ -31,44 +34,55 @@ def main():
     if origen not in lista_origen:
       print("lo sentimos, de momento no tenemos sucursal en tu ciudad")
     else: v2_control = "false"
+  #Creación de una lista de destinos mediante un "for"
+  lista_destino = []
+  for i in lista_origen:
+    if i != origen:
+      lista_destino.append(i) 
   #Tercer while de control
   while v3_control == "true":
+    print(f"Estos son los destinos que tenemos disponibles para ti: {lista_destino}")
     destino = str(input("hacia donde desea viajar?:"))
     destino = destino.lower()
     system("cls")
     if origen == destino:
       print("Por favor introduzca un destino diferente a su ciudad de origen.")
-    elif destino not in lista_origen:
+    elif destino not in lista_destino:
       print("lo sentimos, aun no tenemos ruta para esa ciudad")
-    else: v3_control = "false"
-    
-  lista_destino = []
-  for i in lista_origen:
-    if i != destino:
-      lista_destino.append(i) 
-  print(f"Estos son los destinos que tenemos disponible para ti {lista_destino}")     
+    else: v3_control = "false"    
   if origen == "medellin" and destino == "bogota" or origen == "bogota" and destino == "medellin":
     if origen == "medellin" and destino =="bogota":
-      print("Excelente elección, recuerde llevar abrigo pal' frio")
+      print("Excelente elección!, recuerde llevar abrigo pal' frio.")
     else:
-      print("Excelente elección, le recomendamos visitar el jardín botanico")
+      print("Excelente elección!, le recomendamos visitar el jardín botanico.")
     d = 240
     print(f"entre las ciudades hay una distancia de {d}.")
   elif origen == "medellin" and destino == "cartagena" or origen == "cartagena" and destino == "medellin":
     d = 461
     if origen == "medellin" and destino == "cartagena":
-      print("Excelente elección, recuerde llevar bloqueador pal' sol.")
+      print("Excelente elección!, recuerde llevar bloqueador pal' sol.")
     else:
-      print("Excelente elección, le recomendamos visitar el jardín botanico")
-      
-
-          
+      print("Excelente elección!, le recomendamos visitar el jardín botanico.")
+    print(f"entre las ciudades hay una distancia de {d}.")
+  elif origen == "bogota" and destino == "cartagena" or origen == "cartagena" and destino == "bogota":
+    d = 657
+    if origen ==  "bogota" and destino == "cartagena":
+      print("Excelente elección!, recuerde llevar bloqueador pal' sol.")
+    else: print("Excelente elección!, recuerde llevar abrigo pal' frio.")
+    print(f"entre las ciudades hay una distancia de {d}.")
+  #Cuarto while de control
+  while v4_control == "true":
+    Fecha_vuelo = str(input("Por favor ingrese la fecha en la que quiere agendar su vuelo en formato AA/MM/DÍA (Para ingresar numeros menores que 10 poga un cero antes del numero): "))
+    fecha_control = datetime.today()
+    if Fecha_vuelo < fecha_control:
+      print("por favor ingrese una fecha valida")
+    else: v4_control = "false"
     
           
 
 
 
-pass
+    pass
         
 
 if __name__ == "__main__":
