e5144113996880e6299504c987b7c189bc5e0c97
Nicolas123-Maker <nicolas.bricnom.col@upb.edu.co>
2024-09-14 20:05:38 -0500

    penultima actualizacion del codigo del satelite

--- src/satelite.py
+++ src/satelite.py
@@ -1,32 +1,35 @@
 
 def main():
+    from os import system
     An = float(input("Ingrese la altitud inicial:"))
     As = float(input("ingrese la altitud de seguridad:"))
-    K = float(input("Ingrese el coeficiente de arrastre:"))
-    d = An
-    z = K
-    orbita = 1
-    while d > As:
-        p = K * d
-        d = d - p
-        z += 0.0001
-        orbita += 1
-        if d > As:
-            print(f"en la orbita {orbita}, se tuvo una altitud de {d} y un coeficiente de arrastre de {z} ")
-        if p < 0.000001:
-            d = 0
-            print(f"El satelite se ha estabilizado despues de {orbita} orbitas")
-        elif d < As:
-            print(f"El satelite se ha desintegrado en la atmosfera despues de {orbita} orbitas")
-            opcion = input("le gustaria conocer la altitud en la que el satelite se desintegro? :")
-            opcion = opcion.upper()
-            if opcion not in ["SI", "NO"]:
-                print("opcion no valida ._.")
-            elif opcion == "SI":
-                print(f"el satelie se desintegro a una altitud de {d}")
-            else:
-            
-                pass 
+    if An > As:
+        K = float(input("Ingrese el coeficiente de arrastre:"))
+        d = An
+        z = K
+        orbita = 1
+        system("cls")
+        while d > As:
+            p = K * d
+            d = d - p
+            z += 0.0001
+            orbita += 1
+            if d > As:
+                print(f"en la orbita {orbita}, se tuvo una altitud de {d} y un coeficiente de arrastre de {z} ")
+            if p < 0.000001:
+                d = 0
+                print(f"El satelite se ha estabilizado despues de {orbita} orbitas")
+            elif d < As:
+                print(f"El satelite se ha desintegrado en la atmosfera despues de {orbita} orbitas")
+                opcion = input("le gustaria conocer la altitud en la que el satelite se desintegro? :")
+                opcion = opcion.upper()
+                if opcion not in ["SI", "NO"]:
+                    print("opcion no valida ._.")
+                elif opcion == "SI":
+                    print(f"el satelie se desintegro a una altitud de {d}")
+    else: print("Por favor ingrese una altitud inicial mayor a la de seguridad")
+                
+pass 
 
 
 if __name__ == "__main__":
